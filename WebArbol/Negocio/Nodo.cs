f60953edd57ff1cd3c749d4df456c91cd2eca7e3
Support-Ficct <suport_ficct@hotmail.com>
2016-09-10 18:29:59 -0400

    Add files via upload

---
+++ WebArbol/Negocio/Nodo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class Nodo
+    {
+        int dato;
+        public Nodo()
+        {
+            dato = 100;
+        }
+
+        public String toString(){
+            return "Mi Dato = " + dato;
+        }
+
+
+
+
+
+    }
+}
---
+++ WebArbol/WebArbol/MyFormulario.aspx.cs
@@ -0,0 +1,23 @@
+﻿using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebArbol
+{
+    public partial class MyFormulario : System.Web.UI.Page
+    {
+        Nodo objetoNodo;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ///lo primero que se ejecuta
+            objetoNodo = new Nodo();
+            String resp = objetoNodo.toString();
+            Response.Write(resp);
+        }
+
+    }
+}
